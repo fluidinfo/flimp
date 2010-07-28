8b46234738f8dd94a9e4c61d573afd23f6c58908
Nicholas Tollervey <ntoll@ntoll.org>
2010-07-28 16:49:49 +0100

    Made sure the script handles import errors nicely and updated/fixed some doc related stuff. Bumped to v0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='flimp',
-      version='0.1',
+      version='0.2',
       description='FLuiddb IMPorter - automates importing data into FluidDB',
       author='Nicholas H.Tollervey',
       author_email='ntoll@ntoll.org',
